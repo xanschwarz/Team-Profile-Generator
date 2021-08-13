7a71765b2e11a3fb806b7be076afb79a746981cf
Xander <xanschwarz@gmail.com>
2021-08-12 18:26:53 -0700

    Initial files and folders. Started writing classes.

---
+++ index.js
@@ -0,0 +1,18 @@
+/* ---------------------------------------------------         Developer notes         ------------------------------------------------------
+
+
+Gitignore node modules, anything else not necessary to install app. Remember to start walkthrough video with pre-install setup.
+
+Take user input to accomplish the following:
+
+1. Prompt for team members and their information to generate an HTML file that displays a nicely formatted team roster based on user input.
+2. Clicking on an email address in the HTML causes default email program to open and populates the "TO:" field of the email with the address.
+3. Clicking on the GitHub username opens that GitHub profile in a new tab.
+4. Starting the application (node index.js) begins prompts to enter the team manager’s name, employee ID, email address, and office number.
+5. Upon entering the team manager’s name, employee ID, email address, and office number, app presents a menu with the option to add an engineer
+or an intern or to finish building the team.
+6. Selecting the engineer option prompts to enter the engineer’s name, ID, email, and GitHub username, then takes user back to the menu.
+7. Selecting the intern option prompts to enter the intern’s name, ID, email, and school, then takes user back to the menu.
+8. Selecting "Finish building my team" exits the application, and the HTML is generated.
+
+------------------------------------------------------------------------------------------------------------------------------------------ */
